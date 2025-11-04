0244f3d3538491bfb8dba4792bf82d1b23754c62
jlcoo <longjianguestc@gmail.com>
2025-11-04 18:26:58 +0800

    test: add ut testcase

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 	"github.com/opensourceways/argus-worker/server" // 替换为你的实际 module 名称
 )
 
-func main() {
+// Run 启动应用
+func Run() error {
 	// 启动工作池
 	server.StartWorkerPool()
 	log.Println("Worker 池已启动")
@@ -15,7 +16,11 @@ func main() {
 	// 创建并启动 Gin 服务
 	router := server.NewRouter()
 	log.Println("Web 服务启动于 http://localhost:8080")
-	if err := router.Run(":8080"); err != nil {
+	return router.Run(":8080")
+}
+
+func main() {
+	if err := Run(); err != nil {
 		log.Fatal("服务启动失败: ", err)
 	}
 }
